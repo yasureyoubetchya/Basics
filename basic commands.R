979aa35afb548e4cf9e3952ab4a029474cfe58d0
mjmette1 <mjmette1@gmail.com>
2018-07-31 13:42:11 +0200

    update

--- basic commands.R
+++ basic commands.R
@@ -1,1 +1,11 @@
 getwd()
+
+# read chronology .crn file
+data=read.crn(
+  'https://www1.ncdc.noaa.gov/pub/data/paleo/treering/chronologies/europe/norw007.crn'
+)
+
+crn.plot(data)
+
+m <- morlet(data$KARASJ, x1 = seq_along(data$KARASJ))
+m2 <- wavelet.plot(m)
