43f5716776891589f3248b7a6fb5ca7a23fc4f20
mjmette1 <mjmette1@gmail.com>
2018-07-31 13:42:11 +0200

    update

--- favorite packages.R
+++ favorite packages.R
@@ -1,17 +1,18 @@
 install.packages("ggplot2")
 install.packages("dplR")
-install.packages()
-install.packages()
+install.packages("dplyr")
+install.packages("zoo")
 install.packages()
 install.packages()
 install.packages()
 
+# link for useful packages 
+# https://support.rstudio.com/hc/en-us/articles/201057987-Quick-list-of-useful-R-packages
 
-
-library(ggplot2)
-library(dplR)
-library()
-library()
+library(ggplot2)  # for plotting, obviously!
+library(dplR)     # tree ring analyses
+library(dplyr)    # data manipulation
+library(zoo)      # time series objects
 library()
 library()
 library()
